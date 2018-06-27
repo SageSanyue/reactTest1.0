dc3cfab9ab5e7f79650be19ce4ee1c3fe34c2b30
jirenguSanyue <562441461@qq.com>
2018-06-27 20:11:14 +0800

    基本骨架

--- src/index.js
+++ src/index.js
@@ -1,33 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
-
-//import Welcome from './Welcome'
-
-
-/*function tick(){
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-
-
-  ReactDOM.render(
-      element,
-      document.getElementById('root')
-    );
-
-}
-
-
-setInterval(tick, 1000);
-*/
-
+import App from './App'
 
 ReactDOM.render(
-  <Welcome name="Sage"/>,
+  <App />,
   document.getElementById('root')
 )
