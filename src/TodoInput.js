3bd3c4d15447c0b076a8819276cdeb23d301a4a7
jirenguSanyue <562441461@qq.com>
2018-06-28 09:04:46 +0800

    新增todo

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -7,7 +7,7 @@ export default class TodoInput extends Component {
     submit(e){
         if(e.key === 'Enter'){
             //console.log('用户点击了回车')
-            this.props.onSubmit.call()
+            this.props.onSubmit(e)
         }
     }
 }
